ecaa874fe03763e3d6e9d2db53e0d2489fbac75e
J. Jaime Paredes <jjpp109309@gmail.com>
2023-08-14 21:22:51 -0600

    autodiff

--- src/autodiff/tensor.py
+++ src/autodiff/tensor.py
@@ -1,16 +1,27 @@
 import numpy as np
 
+
 class Tensor:
-    
-    def __init__(self, data):
+
+    def __init__(self, data, creators=None, creation_op=None):
         self.data = np.array(data)
+        self.creation_op = creation_op
+        self.creators = creators
+        self.grad = None
+
+    def backward(self, grad):
+        self.grad = grad
+
+        if self.creation_op == 'add':
+            self.creators[0].backward(grad)
+            self.creators[1].backward(grad)
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        return Tensor(self.data + other.data, creators=[self, other],
+                      creation_op='add')
 
     def __mul__(self, other):
         return Tensor(np.dot(self.data, other.data))
-
 
     def __repr__(self):
         return str(self.data.__repr__())
